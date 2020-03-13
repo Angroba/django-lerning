6eac5e53bfa2da3cc633d8e021204d16b1ed9b90
Angroba <30315654+Angroba@users.noreply.github.com>
2020-03-14 00:27:19 +0300

    реквест погоды с wttr
    создал тестовую прогу для отображения актуальной погоды с wttr используя request с параметрами.
    wttr.in/:help

---
+++ weather.py
@@ -0,0 +1,42 @@
+
+
+import requests
+
+
+cities = [
+    'Омск',
+    'Калининград',
+    'Челябинск',
+    'Владивосток',
+    'Красноярск',
+    'Москва',
+    'Екатеринбург'
+]
+
+
+def make_url(city):
+    # в URL задаём город, в котором узнаем погоду
+    return f'http://wttr.in/{city}'
+
+
+def make_parameters():
+    params = {
+        'format': 2,  # погода одной строкой
+        'M': ''  # скорость ветра в "м/с"
+    }
+    return params
+
+
+def what_weather(city):
+    try:
+        rya=requests.get(make_url(city), make_parameters() )
+ except requests.ConnectionError:
+        return '<сетевая ошибка>'
+    if rya.status_code == 200:
+        return rya.text
+    else:
+        return '<ошибка на сервере погоды>'
+
+print('Погода в городах:')
+for city in cities:
+    print(city, what_weather(city))
