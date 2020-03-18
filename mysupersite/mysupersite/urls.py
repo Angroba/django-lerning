89ce814a781de6309176bc72137f9489c68514d1
Angroba <30315654+Angroba@users.noreply.github.com>
2020-03-18 17:44:48 +0300

    update

--- mysupersite/mysupersite/urls.py
+++ mysupersite/mysupersite/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from star import views as sv
 
 
 
 urlpatterns = [
 
-    path(r'^admin/', admin.site.urls),
-    path(r'^webexample/', include('webexample.urls')),
+    path('admin/', admin.site.urls),
+    path('star', sv.index)
 ]
--- mysupersite/star/views.py
+++ mysupersite/star/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    return HttpResponse("<h3>Привет мир</h3>")
+def index(response):
+    return HttpResponse("<h3>DJAAANGOOOOOOO!</h3>")
